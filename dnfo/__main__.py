0b8c1be7e734f1e7f073c22b20c709b171cbf70c
sudo-julia <jlearning@tuta.io>
2021-03-14 16:28:38 -0700

    arg categories added

--- dnfo/__main__.py
+++ dnfo/__main__.py
@@ -26,8 +26,44 @@ def print_options(results: list):
 def get_arguments() -> argparse.Namespace:
     """get the arguments"""
     parser = argparse.ArgumentParser()
-    categories = parser.add_mutually_exclusive_group()
-    categories.add_argument_group("spells")
+    categories = parser.add_mutually_exclusive_group(required=True)
+    # char data
+    categories.add_argument("ability_score")
+    categories.add_argument("skill")
+    categories.add_argument("proficiencie")
+    categories.add_argument("language")
+    categories.add_argument("alignment")
+    # backgrounds
+    categories.add_argument("background")
+    # classes
+    categories.add_argument("class")
+    categories.add_argument("subclass")
+    categories.add_argument("feature")
+    categories.add_argument("starting_equipment")
+    # races
+    categories.add_argument("race")
+    categories.add_argument("subrace")
+    categories.add_argument("trait")
+    # equipment
+    categories.add_argument("equipment")
+    categories.add_argument("weapon")
+    categories.add_argument("armor")
+    categories.add_argument("gear")
+    categories.add_argument("pack")
+    categories.add_argument("magic_item")
+    categories.add_argument("weapon_properties")
+    # spells
+    categories.add_argument("spells")
+    # monsters
+    categories.add_argument("monster")
+    # game mechanics
+    categories.add_argument("mechanic")
+    categories.add_argument("condition")
+    categories.add_argument("damage")
+    categories.add_argument("magic_school")
+    # rules
+    categories.add_argument("rule")
+    categories.add_argument("rule_section")
 
     return parser.parse_args()
 
---
+++ dnfo/dnd_database/populate_database.py
@@ -0,0 +1,27 @@
+"""create the database of info from https://www.dnd5eapi.co"""
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+
+class Resource:
+    """a resource from the API"""
+
+    ...
+
+
+def create_database():
+    """:"""
+    ...
+
+
+def query_api():
+    """:"""
+    ...
+
+
+def main():
+    """main"""
+    ...
+
+
+if __name__ == "__main__":
+    main()
