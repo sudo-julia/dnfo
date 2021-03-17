6459b127072e3d3ad2239315e676339f8d683d21
sudo-julia <jlearning@tuta.io>
2021-03-16 20:08:11 -0700

    more work towards setting up args

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Search the [DnD5e API](https://www.dnd5eapi.co) and return relevant information
 - [ ] Store responses in a database
   - [ ] Populate the database and create a .lock file, with a regeneration
 if requested by the user
-- [ ] Print rows that fit the size of the command prompt
+- [X] Print rows that fit the size of the command prompt
+- [ ] Store all options in arrays and check arguments against them
+  - [ ] Integrate the above point with the "choices" option for add_argument
--- dnfo/dnfo_args.py
+++ dnfo/dnfo_args.py
@@ -4,91 +4,121 @@ import argparse
 
 def get_arguments() -> argparse.Namespace:
     """get the arguments"""
-    # TODO subparsers, bitch
     # TODO custom help/usage sections
+    # TODO if arg is given with no positional, pull up options
+    #      else, search for the given arg
     parser = argparse.ArgumentParser()
     categories = parser.add_argument_group()
 
     # char data
     categories.add_argument(
-        "ability_scores", help="list available ability scores", action="store_true"
-    )
-    categories.add_argument("skills", help="list available skills", action="store_true")
-    categories.add_argument(
-        "proficiencies", help="list available proficiencies", action="store_true"
-    )
-    categories.add_argument(
-        "languages", help="list available languages", action="store_true"
+        "-a",
+        "--ability_scores",
+        help="list available ability scores",
+        action="store_false",
     )
     categories.add_argument(
-        "alignments", help="list available alignments", action="store_true"
+        "-k", "--skills", help="list available skills", action="store_false"
+    )
+    categories.add_argument(
+        "-p",
+        "--proficiencies",
+        help="list available proficiencies",
+        action="store_false",
+    )
+    categories.add_argument(
+        "-l", "--languages", help="list available languages", action="store_false"
+    )
+    categories.add_argument(
+        "--alignments", help="list available alignments", action="store_false"
     )
     # backgrounds
     categories.add_argument(
-        "backgrounds", help="list available backgrounds", action="store_true"
+        "-b", "--backgrounds", help="list available backgrounds", action="store_false"
     )
     # classes
     categories.add_argument(
-        "classes", help="list available classes", action="store_true"
+        "-c", "--classes", help="list available classes", action="store_false"
     )
     categories.add_argument(
-        "subclasses", help="list available subclasses", action="store_true"
+        "-u", "--subclasses", help="list available subclasses", action="store_false"
     )
     categories.add_argument(
-        "features", help="list available features", action="store_true"
+        "-f", "--features", help="list available features", action="store_false"
     )
     categories.add_argument(
-        "starting_equipment",
+        "-q",
+        "--starting_equipment",
         help="list available starting_equipment",
-        action="store_true",
+        action="store_false",
     )
     # races
-    categories.add_argument("races", help="list available races", action="store_true")
     categories.add_argument(
-        "subraces", help="list available subraces", action="store_true"
+        "--races", help="list available races", action="store_false"
     )
-    categories.add_argument("traits", help="list available traits", action="store_true")
+    categories.add_argument(
+        "--subraces", help="list available subraces", action="store_false"
+    )
+    categories.add_argument(
+        "-t", "--traits", help="list available traits", action="store_false"
+    )
     # equipment
     categories.add_argument(
-        "equipment", help="list available equipment", action="store_true"
+        "-e", "--equipment", help="list available equipment", action="store_false"
     )
     categories.add_argument(
-        "weapons", help="list available weapons", action="store_true"
-    )
-    categories.add_argument("armor", help="list available armor", action="store_true")
-    categories.add_argument("gear", help="list available gear", action="store_true")
-    categories.add_argument("packs", help="list available packs", action="store_true")
-    categories.add_argument(
-        "magic_items", help="list available magic_items", action="store_true"
+        "-w", "--weapons", help="list available weapons", action="store_false"
     )
     categories.add_argument(
-        "weapon_properties",
+        "--armor", help="list available armor", action="store_false"
+    )
+    categories.add_argument(
+        "-g", "-gear", help="list available gear", action="store_false"
+    )
+    categories.add_argument(
+        "--packs", help="list available packs", action="store_false"
+    )
+    categories.add_argument(
+        "--magic_items", help="list available magic_items", action="store_false"
+    )
+    categories.add_argument(
+        "--weapon_properties",
         help="list available weapon_properties",
-        action="store_true",
+        action="store_false",
     )
     # spells
-    categories.add_argument("spells", help="list available spells", action="store_true")
+    categories.add_argument(
+        "-s",
+        "--spells",
+        nargs="?",
+        const="list",
+        help="""without an argument, list all available spells.
+                            otherwise, display spell info""",
+        type=str,
+        metavar="SPELL",
+    )
     # monsters
     categories.add_argument(
-        "monsters", help="list available monsters", action="store_true"
+        "-m", "--monsters", help="list available monsters", action="store_false"
     )
     # game mechanics
     categories.add_argument(
-        "mechanics", help="list available mechanics", action="store_true"
+        "-o", "--conditions", help="list available conditions", action="store_false"
     )
     categories.add_argument(
-        "conditions", help="list available conditions", action="store_true"
+        "-d", "--damages", help="list available damages", action="store_false"
     )
     categories.add_argument(
-        "damages", help="list available damages", action="store_true"
-    )
-    categories.add_argument(
-        "magic_schools", help="list available magic_schools", action="store_true"
+        "--magic_schools",
+        help="list available magic_schools",
+        action="store_false",
     )
     # rules
-    categories.add_argument("rules", help="list available rules", action="store_true")
     categories.add_argument(
-        "rule_sections", help="list available rule_sections", action="store_true"
+        "-r", "--rules", help="list available rules", action="store_false"
+    )
+    categories.add_argument(
+        "--rule_sections", help="list available rule_sections", action="store_false"
     )
 
     return parser.parse_args()
