db0b9010505cece93bce13a6f48cc1c21125169b
sudo-julia <jlearning@tuta.io>
2021-03-28 17:19:18 -0700

    db commit hash is now checked

--- dnfo/get_database/__init__.py
+++ dnfo/get_database/__init__.py
@@ -14,13 +14,31 @@ import git
 # TODO should i just be downloading the repo lmfao
 
 
-def download_db(location: str):
-    """make the temporary directory to download files to"""
+def download_db(url: str, location: str) -> str:
+    """clone a repo and return the hash of HEAD"""
     print("Cloning database repo...")
-    git.Repo.clone_from("https://github.com/5e-bits/5e-database", location)
+    repo: git.Repo = git.Repo.clone_from(url, location)
+    hexsha: str = repo.head.object.hexsha
+    return hexsha
 
 
-def copy_files(source: str, dest: str):
+def hashes_match(hash_file: Path, newhash: str) -> bool:
+    """compare the hash of recently pulled repo with the stored hash
+    returns True if hashes match. returns False if the hashes are different.
+    """
+    try:
+        with open(hash_file, "r") as file:
+            oldhash: str = file.read().strip()
+        if oldhash != newhash:
+            return False
+    except FileNotFoundError:
+        with open(hash_file) as file:
+            file.write(newhash.strip())
+    finally:
+        return True
+
+
+def copy_json(source: str, dest: str):
     """move files to data directory"""
     errors: list[str] = []
 
@@ -49,13 +67,19 @@ def populate_db():
     appauthor: str = "sudo_julia"
     data_dir: str = appdirs.user_data_dir(appname, appauthor)  # path to data dir
     db_dir: str = f"{data_dir}/db"  # path to database dir
+    hash_file: Path = Path(f"{data_dir}/old_HEAD")
+    url: str = "https://github.com/5e-bits/5e-database"
 
     try:
-        print(f"Creating '{db_dir}' for database storage.")
+        print(f"Creating '{db_dir}' for database storage...")
         Path(db_dir).mkdir(parents=True)
     except FileExistsError:
         print(f"'{db_dir}' already exists.")
 
     with tempfile.TemporaryDirectory(prefix="dnfo.") as tmpdir:
-        download_db(tmpdir)
-        copy_files(tmpdir, db_dir)
+        head_hash = download_db(url, tmpdir)
+        if hashes_match(hash_file, head_hash):
+            # TODO option to overwrite
+            print("Database is already up to date. Cancelling operation.")
+            return
+        copy_json(tmpdir, db_dir)
