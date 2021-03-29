c93a91e730d6013aee442b12bd517284d59e5717
sudo-julia <jlearning@tuta.io>
2021-03-28 18:03:39 -0700

    hash checking works!!

--- dnfo/get_database/__init__.py
+++ dnfo/get_database/__init__.py
@@ -4,14 +4,13 @@ import json
 import shutil
 import tempfile
 from pathlib import Path
+from typing import Generator
 import appdirs
 import git
 
 
 # TODO progress bar!!
 # TODO download older release if database breaks
-# TODO log git commmit in data_dir
-# TODO should i just be downloading the repo lmfao
 
 
 def download_db(url: str, location: str) -> str:
@@ -22,33 +21,36 @@ def download_db(url: str, location: str) -> str:
     return hexsha
 
 
-def hashes_match(hash_file: Path, newhash: str) -> bool:
+def hashes_match(hash_file: Path, newhash: str, force=None) -> bool:
     """compare the hash of recently pulled repo with the stored hash
     returns True if hashes match. returns False if the hashes are different.
     """
     try:
         with open(hash_file, "r") as file:
             oldhash: str = file.read().strip()
-        if oldhash != newhash:
+        if oldhash == newhash and not force:
             return False
     except FileNotFoundError:
-        with open(hash_file) as file:
-            file.write(newhash.strip())
+        pass
     finally:
-        return True
+        with open(hash_file, "w") as file:
+            file.write(newhash.strip())
+        return True  # pylint: disable=W0150
 
 
-def copy_json(source: str, dest: str):
+def copy_json(source: str, dest: str) -> bool:
     """move files to data directory"""
-    errors: list[str] = []
-
     print("Copying files to local database...")
-    src_dir = Path(source)
-    for file in src_dir.glob("*.json"):
+    src_dir = Path(f"{source}/src")
+    json_files: Generator = src_dir.glob("*.json")
+
+    for file in json_files:
+        # TODO display errors properly
         if not validate_json(file):
-            errors.append(f"'{file}' does not contain valid JSON data.")
-            continue
-        shutil.copyfile(file, dest)
+            print(f"'{file}' does not contain valid JSON data.\nAborting operation.")
+            return False
+        shutil.copyfile(file, f"{dest}/{file.name}")
+    return True
 
 
 def validate_json(file: Path) -> bool:
@@ -61,25 +63,35 @@ def validate_json(file: Path) -> bool:
     return True
 
 
-def populate_db():
+def populate_db(name: str, author: str) -> int:
     """perform the bulk of the operations"""
-    appname: str = "dnfo"
-    appauthor: str = "sudo_julia"
-    data_dir: str = appdirs.user_data_dir(appname, appauthor)  # path to data dir
+    data_dir: str = appdirs.user_data_dir(name, author)  # path to data dir
     db_dir: str = f"{data_dir}/db"  # path to database dir
     hash_file: Path = Path(f"{data_dir}/old_HEAD")
-    url: str = "https://github.com/5e-bits/5e-database"
+    url: str = "https://github.com/5e-bits/5e-database"  # url for database
 
     try:
         print(f"Creating '{db_dir}' for database storage...")
         Path(db_dir).mkdir(parents=True)
     except FileExistsError:
-        print(f"'{db_dir}' already exists.")
+        print("Using existing database.")
 
     with tempfile.TemporaryDirectory(prefix="dnfo.") as tmpdir:
         head_hash = download_db(url, tmpdir)
         if hashes_match(hash_file, head_hash):
             # TODO option to overwrite
             print("Database is already up to date. Cancelling operation.")
-            return
-        copy_json(tmpdir, db_dir)
+            return 0
+        if not copy_json(tmpdir, db_dir):
+            print("Error copying JSON.")
+            return 1
+    return 0
+
+
+def clear_db():
+    """clear the database to be repopulated"""
+    ...
+
+
+if __name__ == "__main__":
+    populate_db("dnfo", "sudo_julia")
